25e488bfbf700509c3c43284524c0b33ff5e7ecc
Olena Parakonna <166116539+Parakonna@users.noreply.github.com>
2024-09-22 21:16:23 +0200

    css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -5,7 +5,7 @@ const FriendListItem = ({ name, isOnline, avatar }) => {
     <div className={style.conteiner}>
           <img className={style.img} src={avatar} alt="Avatar" width="48" />
           <p className={style.name}>{name}</p>
-          {isOnline ? <p style={{color: "darkgreen"}}>Online</p> : <p style={{color: "red"}}>Offline</p>}
+          {isOnline ? <p className={style.online}>Online</p> : <p className={style.offline}>Offline</p>}
 </div>
   )
 }
